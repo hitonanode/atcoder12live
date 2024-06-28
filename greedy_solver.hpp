9a18012b0962ceff8680efadd11ba9870a52f41c
Ryotaro Sato <ryotaro@mcdigital.jp>
2024-06-28 14:31:39 +0900

    faster greedy

--- greedy_solver.hpp
+++ greedy_solver.hpp
@@ -7,37 +7,49 @@
 Solution greedy_solve(const Instance &ins) {
     Solution ans(T, {0, 1, 0});
 
-    std::vector<int> state(N);
+    std::array<int, N> state;
+    state.fill(0);
+
+    std::array<int, N> diffs;
 
     for (int t = 0; t < T; ++t) {
         int best_l = -1, best_r = -1, best_dv = 0, best_eval = 1 << 30;
 
-        for (int l = 0; l < N; ++l) {
-            for (int r = l + 1; r <= N; ++r) {
-                for (int dv = -5; dv <= 5; ++dv) {
-                    if (dv == 0) continue;
+        for (int dv = -5; dv <= 5; ++dv) {
+            if (dv == 0) continue;
 
-                    int e = 0;
-                    for (int i = 0; i < N; ++i) {
-                        const int c = state.at(i) + (i >= l and i < r ? dv : 0);
+            for (int i = 0; i < N; ++i) {
+                int e = 0;
+                const int c = state.at(i) + dv;
 
-                        e += std::abs(ins.val(t, i) - c) * 8;
-                        if (t + 1 < T) e += std::abs(ins.val(t + 1, i) - c) * 4;
-                        if (t + 2 < T) e += std::abs(ins.val(t + 2, i) - c) * 2;
-                        if (t + 3 < T) e += std::abs(ins.val(t + 3, i) - c) * 1;
-                    }
+                constexpr int UB = 4;
+                for (int turn = 0; turn < UB; ++turn) {
+                    if (t + turn >= T) break;
+                    const int u = std::abs(ins.val(t + turn, i) - c) - std::abs(ins.val(t + turn, i) - state.at(i));
+                    e += u * (1 << (UB - turn - 1));
+                }
 
-                    if (chmin(best_eval, e)) {
-                        best_l = l;
-                        best_r = r;
-                        best_dv = dv;
-                    }
+                diffs.at(i) = e;
+            }
+
+            int cs = 0;
+            int max_cs = 0;
+            int arg_maxcs = 0;
+            for (int i = 0; i < N; ++i) {
+                cs += diffs.at(i);
+                if (chmax(max_cs, cs)) {
+                    arg_maxcs = i + 1;
+                }
+                if (chmin(best_eval, cs - max_cs)) {
+                    best_l = arg_maxcs;
+                    best_r = i + 1;
+                    best_dv = dv;
                 }
             }
         }
 
         ans.at(t) = {best_l, best_r, best_dv};
-        for (int i = best_l; i < best_r; ++i) { state.at(i) += best_dv; }
+        for (int i = best_l; i < best_r; ++i) state.at(i) += best_dv;
     }
 
     return ans;
